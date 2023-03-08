2fe66f6ab26033a082ed3a46223799e8d51610f2
Rafael Puerta <0x14rp@Rafaels-MBP.lan>
2023-03-07 22:08:28 -0300

    init files

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+
+return {
+  tab_max_width = 16,
+  text_background_opacity = 0.7,
+  hide_tab_bar_if_only_one_tab = false,
+  font_size = 16,
+  window_background_opacity = 0.75,
+  font = wezterm.font('Fira code'),
+  color_scheme = "Catppuccin Mocha",
+  animation_fps = 1,
+  cursor_blink_ease_in = 'Constant',
+  cursor_blink_ease_out = 'Constant',
+  --  tab_bar_at_bottom = true,
+}
