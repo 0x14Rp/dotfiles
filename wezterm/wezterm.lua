5e6ff8ae60fe80c783d01fbdf0f2bf11c48a7fb9
0x14Rp <rpuertabraca@gmail.com>
2023-04-01 15:08:37 -0300

    fix padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,12 +4,21 @@ return {
   tab_max_width = 16,
   text_background_opacity = 0.7,
   hide_tab_bar_if_only_one_tab = false,
-  font_size = 16,
-  window_background_opacity = 0.75,
+  font_size = 14,
   font = wezterm.font('Fira code'),
   color_scheme = "Catppuccin Mocha",
   animation_fps = 1,
   cursor_blink_ease_in = 'Constant',
   cursor_blink_ease_out = 'Constant',
-  tab_bar_at_bottom = true,
+  window_background_opacity = 0.3,
+  macos_window_background_blur = 20,
+
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  }
+
+
 }
