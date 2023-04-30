4587a03b14b42ca3ff31e1c37dc414cd5961a737
0x14Rp <rpuertabraca@gmail.com>
2023-04-30 00:00:45 -0400

    blur effect

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,15 @@
 local wezterm = require 'wezterm'
 
+wezterm.on("format-window-title", function(tab)
+  local pane = tab.active_pane
+  local title = pane.title
+  local emoji = 'R ツ'
+  if pane.domain_name then
+    title = emoji
+  end
+  return title
+end)
+
 return {
   tab_max_width = 16,
   text_background_opacity = 0.7,
@@ -10,15 +20,15 @@ return {
   animation_fps = 1,
   cursor_blink_ease_in = 'Constant',
   cursor_blink_ease_out = 'Constant',
-  window_background_opacity = 0.3,
+  window_background_opacity = 0.7,
   macos_window_background_blur = 20,
-
+  enable_tab_bar = false,
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
-  }
+  },
 
 
 }
