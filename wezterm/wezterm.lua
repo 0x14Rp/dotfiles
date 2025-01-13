10c670512ce8fdf6c8bc65481ef7f1fe82a0a0bf
0x14Rp <rpuertabraca@gmail.com>
2025-01-12 22:42:23 -0300

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,62 @@
+
+local wezterm = require("wezterm")
+
+local config = {}
+local mocha = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+local username = os.getenv("USER") or os.getenv("USERNAME") or "unknown"
+config.color_scheme = "Catppuccin Mocha"  
+config.colors = {
+  cursor_border = "#74c7ec",
+  cursor_bg = "#74c7ec",
+  cursor_fg = "#11111b",
+}
+config.font = wezterm.font_with_fallback({
+  { family = "CaskaydiaCove Nerd Font" },
+  { family = "FantasqueSansM Nerd Font" },
+})
+config.font_size = 12.0  
+
+config.initial_cols = 120
+config.initial_rows = 30
+
+config.window_background_opacity = 0.9
+
+config.window_decorations = "RESIZE"
+
+config.window_close_confirmation = "AlwaysPrompt"
+
+config.scrollback_lines = 3000
+
+config.default_workspace = "home"
+
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5,
+}
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 3000 }
+
+config.keys = {}
+
+config.key_tables = {}
+
+
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+config.tab_bar_at_bottom = true
+
+
+
+wezterm.on("update-right-status", function(window, pane)
+	local date = username .. " " 
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "⚡" .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+  window:set_right_status(wezterm.format({
+		{Text=bat ..  "   "},
+	}));
+end)
+
+return config
+
